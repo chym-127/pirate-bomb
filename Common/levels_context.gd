2d04d0294cbc496ce028a91ffb6982aa55b1b283
chenwei <294098494@qq.com>
2023-03-31 18:37:56 +0800

    第二关

--- Common/levels_context.gd
+++ Common/levels_context.gd
@@ -13,10 +13,16 @@ enum {
 var enemy_count = 3
 var door_state = CLOSED
 var level = 1
+var next_level_scene = null
+
+var level_format = "res://Scenes/level_{index}.tscn"
+
 
 func set_state(enemy_count,level):
 	self.level = level
 	self.enemy_count = enemy_count
+	var str = level_format.format([["index",level+1]])
+	next_level_scene = load(str)
 
 func enemy_death():
 	self.enemy_count -= 1
@@ -28,7 +34,8 @@ func open_door():
 	emit_signal("opendoor")
 	
 func next_level():
-	print("next_level")
+	if next_level_scene:
+		get_tree().change_scene_to_packed(next_level_scene)
 
 func door_is_closed():
 	next_level()
--- Scenes/menu_scene.gd
+++ Scenes/menu_scene.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var l_1 = preload("res://Scenes/world.tscn")
+var l_1 = preload("res://Scenes/level_1.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
